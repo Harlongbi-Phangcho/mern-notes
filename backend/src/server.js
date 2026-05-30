0876a49993ff50e488d36261362dc23ceb93c8eb
Harlongbi-Phangcho <harbisonphangcho1999@gmail.com>
2026-05-30 08:50:37 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,7 +3,8 @@ import express from "express";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import { rateLimiter } from "./middleware/rateLimiter.js";
-import cors from "cors"
+import cors from "cors";
+import path from "path";
 
 dotenv.config();
 
@@ -11,12 +12,18 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 //middleware
-app.use(cors({
-  origin: "http://localhost:5173"
-}))
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    }),
+  );
+}
+
 app.use(express.json());
 app.use(rateLimiter);
 
+const __dirname = path.resolve();
 
 // importing routes
 import noteRoutes from "./routes/noteRoutes.js";
@@ -24,6 +31,13 @@ import noteRoutes from "./routes/noteRoutes.js";
 // declaring routes
 app.use("/api/v1/notes", noteRoutes);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {
